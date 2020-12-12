1f8f764100b0a7d3d416906e03ce0c3f58c57e06
palmcm <palmermonkeyboy@gmail.com>
2020-12-13 06:27:19 +0700

    Setup Config and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  
+  const options = new DocumentBuilder()
+  .setTitle('RNKM 63 API')
+  .setDescription('Doc for API')
+  .setVersion('1.0')
+  .addTag('user')
+  .addTag('auth')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  
+  app.use(cookieParser());
   app.setGlobalPrefix('api')
-  await app.listen(3000);
+  await app.listen(configService.get("port"));
 }
 bootstrap();
