e7b01dae7e9e33c5f4823b938fefe31a33a38037
palmcm <palmermonkeyboy@gmail.com>
2020-12-13 06:27:19 +0700

    Setup Config and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(
+    {
+      isGlobal: true,
+      load: [configuration],
+    }
+  ), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
