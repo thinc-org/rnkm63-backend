f4567a34caa002a8c3bb144cf3680785e6836718
palmcm <palmermonkeyboy@gmail.com>
2020-12-16 16:47:56 +0700

    Add env var origin+ certificate

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,12 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  app.enableCors();
+  if (configService.get('cors.use')) {
+    app.enableCors({
+      origin: configService.get('cors.origin'),
+      credentials: true,
+    });
+  }
 
   await app.listen(configService.get('port'));
 }
