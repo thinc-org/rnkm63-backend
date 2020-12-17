169b714aa6c291f755fbe7b84d36b5ff837fb503
PongDev <pongwiwat.dev@outlook.com>
2020-12-17 11:44:09 +0700

    Global Config [Manual Cache - Better Ver.]

--- src/global/global.controller.ts
+++ src/global/global.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { GlobalService } from './global.service';
-import { GlobalConfigDTO } from './dto/global-config.dto';
+import { Global } from './global.entity';
 
 @Controller('global')
 export class GlobalController {
   constructor(private globalService: GlobalService) {}
 
   @Get('config')
-  async getConfig(): Promise<GlobalConfigDTO> {
+  async getConfig(): Promise<Global> {
     return await this.globalService.getGlobal();
   }
 }
--- src/global/global.service.ts
+++ src/global/global.service.ts
@@ -2,65 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Global } from './global.entity';
-import { GlobalConfigDTO } from './dto/global-config.dto';
-import pMemoize = require('p-memoize');
-
-let globalConfig: GlobalConfigDTO = null;
-let globalConfigTimeStamp: number = null;
 
 @Injectable()
 export class GlobalService {
+  private cacheGlobalConfig: Global = null;
+  private cacheTimeStamp: number = 0;
+
   constructor(
     @InjectRepository(Global)
     private globalRepository: Repository<Global>,
   ) {}
 
-  async getGlobal(): Promise<GlobalConfigDTO> {
-    if (
-      globalConfig === null ||
-      new Date().getTime() - globalConfigTimeStamp >= 10000
-    ) {
-      console.log('Load Config From DB ' + new Date());
-      globalConfigTimeStamp = new Date().getTime();
-      let loadConfig = await this.globalRepository.findOne(1);
-      if (typeof loadConfig === 'undefined') {
-        await this.createGlobal();
-        loadConfig = await this.globalRepository.findOne(1);
-      }
-      globalConfig = {
-        isDown: loadConfig.isDown,
-        downReasonEn: loadConfig.downReasonEn,
-        downReasonTh: loadConfig.downReasonTh,
-        roundCount: loadConfig.roundCount,
-        phaseCount: loadConfig.phaseCount,
-      };
-    }
-    return globalConfig;
-  }
-
-  // async getGlobal(): Promise<GlobalConfigDTO> {
-  //   let globalConfig: Global;
-  //   const memFunction = async () => {
-  //     console.log('Load Config From DB ' + new Date());
-  //     globalConfig = await this.globalRepository.findOne(1);
-  //     if (typeof globalConfig === 'undefined') {
-  //       await this.createGlobal();
-  //       globalConfig = await this.globalRepository.findOne(1);
-  //     }
-  //   };
-  //   pMemoize(memFunction, { maxAge: 10000 });
-  //   await memFunction();
-  //   return {
-  //     isDown: globalConfig.isDown,
-  //     downReasonEn: globalConfig.downReasonEn,
-  //     downReasonTh: globalConfig.downReasonTh,
-  //     roundCount: globalConfig.roundCount,
-  //     phaseCount: globalConfig.phaseCount,
-  //   };
-  // }
-
-  async createGlobal() {
-    console.log('Create Global Config');
+  async createGlobal(): Promise<Global> {
+    console.log('Create Global Config ' + new Date());
     const global = new Global();
 
     global.isDown = false;
@@ -68,6 +22,18 @@ export class GlobalService {
     global.downReasonTh = null;
     global.roundCount = 0;
     global.phaseCount = 0;
-    await this.globalRepository.save(global);
+    return await this.globalRepository.save(global);
+  }
+
+  async getGlobal(): Promise<Global> {
+    if (new Date().getTime() - this.cacheTimeStamp >= 10000) {
+      console.log('Load Config From DB ' + new Date());
+      this.cacheTimeStamp = new Date().getTime();
+      const globalConfig = await this.globalRepository.findOne(1);
+      if (typeof globalConfig === 'undefined')
+        this.cacheGlobalConfig = await this.createGlobal();
+      else this.cacheGlobalConfig = globalConfig;
+    }
+    return this.cacheGlobalConfig;
   }
 }
