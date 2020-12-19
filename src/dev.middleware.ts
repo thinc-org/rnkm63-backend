a39b02cc423fd1101f1dcdb8adfe8ea80175096f
PongDev <pongwiwat.dev@outlook.com>
2020-12-19 22:24:22 +0700

    Implement Dev Middleware

---
+++ src/dev.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware, HttpStatus } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DevMiddleware implements NestMiddleware {
+  private isInDev: boolean = false;
+  constructor(private configService: ConfigService) {
+    this.isInDev = this.configService.get<boolean>('inDev');
+  }
+
+  use(req: any, res: any, next: () => void) {
+    if (!this.isInDev) {
+      res.statusCode = HttpStatus.FORBIDDEN;
+      res.json({
+        statusCode: HttpStatus.FORBIDDEN,
+      });
+    } else next();
+  }
+}
