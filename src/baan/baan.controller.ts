f83843e8371589f90d708b5b1c9ae4941b94337b
PongDev <pongwiwat.dev@outlook.com>
2020-12-22 20:57:23 +0700

    Fix as eslint recommended

--- src/baan/baan.controller.ts
+++ src/baan/baan.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { BaanService } from './baan.service';
-import { Baan } from './baan.entity';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { HeaderGuard } from '../auth/header.guard';
 
 @ApiTags('baan')
 @Controller('baan')
