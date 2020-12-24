c386a0741a758e71c7109e79e6b9b10c3c5bfd15
PongDev <pongwiwat.dev@outlook.com>
2020-12-24 21:05:46 +0700

    Edit Assignment Log Table

--- src/assignment/assignment_log.entity.ts
+++ src/assignment/assignment_log.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Index,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class AssignmentLog {
@@ -16,8 +22,14 @@ export class AssignmentLog {
   preferBaan: number | null;
 
   @Column()
+  assignedBaan: number;
+
+  @Column()
   round: number;
 
   @Column()
   result: boolean;
+
+  @CreateDateColumn()
+  createAt: number;
 }
