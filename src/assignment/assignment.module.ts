2a9914c41809cfaa97d566814059f3aa33c17054
PongDev <pongwiwat.dev@outlook.com>
2020-12-26 02:16:33 +0700

    Update And Use Phase Guard To Route

--- src/assignment/assignment.module.ts
+++ src/assignment/assignment.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssignmentService } from './assignment.service';
 import { AssignmentController } from './assignment.controller';
 import { AssignmentLog } from './assignment_log.entity';
+import { GlobalModule } from '../global/global.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AssignmentLog])],
+  imports: [TypeOrmModule.forFeature([AssignmentLog]), GlobalModule],
   providers: [AssignmentService],
   controllers: [AssignmentController],
 })
