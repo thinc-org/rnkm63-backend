12ae545ab81a28e678a3c250b86031a21e03bdcf
PongDev <pongwiwat.dev@outlook.com>
2021-01-02 22:19:52 +0700

    Add Cron Job To Set Phase 3

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -7,7 +7,12 @@ export class CronService {
   constructor(private globalService: GlobalService) {}
 
   @Cron(new Date('2021-01-09T12:00:00+07:00'))
-  setGlobalPhaseCron() {
+  setGlobalPhaseTo2() {
     this.globalService.setGlobalPhase(2);
+  }
+
+  @Cron(new Date('2021-01-13T12:15:00+07:00'))
+  setGlobalPhaseTo3() {
+    this.globalService.setGlobalPhase(3);
   }
 }
